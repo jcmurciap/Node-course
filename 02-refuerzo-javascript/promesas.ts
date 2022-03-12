3f1711e5a604558c2d9c0a2a4c1d65ae6a565839
enigma <camilo.murcia.pimentel@gmail.com>
2022-03-11 21:37:31 -0500

    Add promesas

---
+++ 02-refuerzo-javascript/promesas.ts
@@ -0,0 +1,58 @@
+const empleadoss = [
+    {
+        id:1,
+        nombre:"Juan"
+    },
+    {
+        id:2,
+        nombre:"Camilo"
+    },
+    {
+        id:3,
+        nombre:"Sergio"
+    }
+];
+
+const salarioss = [
+    {
+        id: 1,
+        salario: 1000,
+    },
+    {
+        id: 2,
+        salario: 2000,
+    },
+    {
+        id: 3,
+        salario: 3000,
+    }
+];
+
+const getEmpleadoo = (id:number) => {
+    
+    return new Promise((resolve, reject) => {
+        const employee = empleadoss.find(e => e.id === id)?.nombre;
+        
+        (employee)
+            ?resolve(employee)
+            :reject(`Empleado con el id ${id} no existe`)  
+    })
+}
+
+const getSalarioo = (id:number) => {
+    return new Promise((resolve, reject) => {
+        const salario = salarioss.find(s => s.id === id)?.salario;
+        (salario)
+            ?resolve(salario)
+            :reject(`Salario con el id ${id} no existe`);
+    })
+}
+
+const id = 1;
+getEmpleadoo(id)
+    .then(empleado => console.log(empleado)) // si la promesa es procesada
+    .catch(err => console.log(err));         // manejo de errores
+
+getSalarioo(id)
+    .then(salario => console.log(salario))
+    .catch(err => console.log(err));
